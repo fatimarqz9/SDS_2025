2a824246b835209ec41379a72654149646868f12
fatimarqz9 <fam04marquez@gmail.com>
2025-10-16 23:16:53 -0600

    Información dia 3

--- MVC/app/views/DayThree_View.php
+++ MVC/app/views/DayThree_View.php
@@ -6,6 +6,27 @@
     <title>Document</title>
 </head>
 <body>
-    
+        <nav class="nav">
+        <div class="logo">SDS<span>25</span></div>
+        <ul>
+            <li><a href="../public">Home</a></li>
+            <li><a href="dia1">Día 1</a></li>
+            <li><a href="dia2">Día 2</a></li>
+            <li><a href="dia3">Día 3</a></li>
+            <li><a href="dia4">Día 4</a></li>
+            <li><a href="dia5">Día 5</a></li>
+            <li><a href="perfil">Perfil</a></li>
+            <li><a href="registrarVisita">Registrar visita</a></li>
+        </ul>
+    </nav>
+    <section class="dia_presentation">
+        <div class="dia_intro">
+            <h1>Día 3: Torneo de Fultbol</h1>
+            <p style="color: red; font-weight: bold;">CANCELADO POR LLUVIAS</p>
+        </div>
+        <div class="dia_img">
+            <img src="../public/img/image.png" alt="">
+        </div>
+    </section>
 </body>
 </html>
